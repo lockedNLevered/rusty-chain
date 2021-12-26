1e4cb4d601d696f4c3f766d3fc822f7bd940a018
lockedNLevered <shaynel@sfu.ca>
2021-12-25 17:02:16 -0800

    can send transactions

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 use time::OffsetDateTime;
-#[derive(Debug)]
-#[derive(Hash)]
+#[derive(Debug, Hash, Clone)]
 struct Transaction {
     sender: String,
     recipient: String,
     amount: u8,
 }
 
-#[derive(Debug)]
-#[derive(Hash)]
+#[derive(Debug, Hash, Clone)]
 struct Block {
     index: usize,
     timestamp: OffsetDateTime,
     transactions: Vec<Transaction>,
+    previous_hash: u64,
+    proof: u8,
 }
 
 trait ExposeDetails {
@@ -34,31 +34,47 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
 }
 #[derive(Debug)]
 struct BlockChain {
+    current_transactions: Vec<Transaction>,
     chain: Vec<Block>,
 }
 
 impl BlockChain {
     fn new() -> Self {
         let mut chain = Vec::new();
-        chain.push(Block {
-            index: 1,
+
+        let genesis_block = Block {
+            index: 0,
             timestamp: OffsetDateTime::now_utc(),
             transactions: Vec::new(),
-        });
-        BlockChain { chain }
+            previous_hash: 0000000000,
+            proof: 1,
+        };
+        chain.push(genesis_block);
+        BlockChain {
+            chain,
+            current_transactions: Vec::new(),
+        }
     }
-    fn last_block(&self) -> Option<&Block> {
-        self.chain.last()
+    fn new_transaction(&mut self, sender: String, recipient: String, amount: u8) -> usize {
+        let transaction = Transaction {
+            sender,
+            recipient,
+            amount,
+        };
+        self.current_transactions.push(transaction);
+        let next_block = self.last_block().clone().get_index();
+        return next_block;
+    }
+    fn last_block(&self) -> &Block {
+        self.chain.last().unwrap()
     }
 }
 
 fn main() {
-    let chain = BlockChain::new();
+    let mut chain = BlockChain::new();
     let block = chain.last_block();
-    if let Some(block) = block {
-        println!("Hello");
-        
-        println!("{:?}", calculate_hash(&block));
-    }
+    println!("{:?}", calculate_hash(&block));
+    chain.new_transaction(String::from("shayne"), String::from("bob"), 5);
+    chain.new_transaction(String::from("thomas"), String::from("wayne"), 51);
     println! {"{:?}", chain}
 }
