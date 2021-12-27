ad582731d4e131dd0cfa3777c18f04b7604a3688
lockedNLevered <shaynel@sfu.ca>
2021-12-26 18:42:23 -0800

    can post new entries to the block chain from react client

---
+++ client/src/App.tsx
@@ -0,0 +1,63 @@
+import { SyntheticEvent, useEffect, useState } from "react";
+import axios from "axios";
+import { Block } from "../../pkg/rusty_chain";
+import "./index.css";
+const API_URL = "http://localhost:4000";
+function App() {
+	const [chain, setChain] = useState<Block[] | null>(null);
+	const [sender, setSender] = useState("");
+	const [recipient, setRecipient] = useState("");
+	const [amount, setAmount] = useState(0);
+	async function fetchApi() {
+		const res = await axios.get(API_URL);
+		setChain(res.data);
+	}
+	useEffect(() => {
+		fetchApi();
+	}, []);
+
+	async function handleSubmit(e: SyntheticEvent<HTMLFormElement>) {
+		const data = {
+			recipient,
+			sender,
+			amount,
+		};
+
+		const res = await axios.post(API_URL, data);
+		console.log(res);
+		return fetchApi();
+	}
+
+	console.log(chain);
+	return (
+		<div className="app">
+			<form className="form" onSubmit={handleSubmit}>
+				<label htmlFor="sender">From</label>
+				<input
+					value={sender}
+					onChange={(e) => setSender(e.target.value)}
+					name="sender"
+				/>
+				<label htmlFor="recipient">To</label>
+				<input
+					value={recipient}
+					onChange={(e) => setRecipient(e.target.value)}
+					name="recipient"
+				/>
+				<label htmlFor="amount">Amount</label>
+				<input
+					value={amount}
+					onChange={(e) => setAmount(parseInt(e.target.value))}
+					name="amount"
+					type="number"
+				/>
+				<button type="submit">Add Transaction to the BlockChain</button>
+			</form>
+			<div className="data">
+				{chain && <pre>{JSON.stringify(chain, null, 4)}</pre>}
+			</div>
+		</div>
+	);
+}
+
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,29 @@
 import express from "express";
-import * as wasm from "../../pkg/rusty_chain";
-
-interface Transaction {
-	sender: string;
-	reciever: string;
-	amount: number;
-}
+import cors from "cors";
+import { BlockChain, init_panic_hook } from "../../pkg/rusty_chain";
+import { Transaction } from "./types";
 
 const app = express();
 //Initialize logging of rust panics
-wasm.init_panic_hook();
+init_panic_hook();
 //create a new block chain on launch
-const chain = new wasm.BlockChain();
+const chain = new BlockChain();
+app.use(cors());
 app.use(express.json());
-
+//Endpoint to the view the blockchain -> this does not persist so it will be empty on launch
 app.get("/", (_, res) => {
 	res.json(chain.get_chain());
 });
+//Endpoint to create a new transaction -> for simplicity, this endpoint
+//also mines a new block via our proof of work algorithm
 app.post("/", (req, res) => {
 	console.log(req);
-	const { sender, reciever, amount }: Transaction = req.body;
-	const newTransaction = chain.new_transaction(sender, reciever, amount);
+	const { sender, recipient, amount }: Transaction = req.body;
+	const newTransaction = chain.new_transaction(sender, recipient, amount);
 	const last_proof = chain.last_block().get_proof();
 	const proof = chain.proof_of_work(last_proof);
 	chain.new_block(proof);
 	res.status(201).json(newTransaction);
 });
 
-app.listen(3000, () => console.log("live"));
+app.listen(4000, () => console.log("live"));
